d1c2a7e366d6517bf0320c57f9caa841652a9c57
Rui Constancio <r.miguel.a.c@gmail.com>
2022-09-11 12:44:25 -0300

    Add words to the random selection

--- src/components/KanaDisplay/index.js
+++ src/components/KanaDisplay/index.js
@@ -1,17 +1,19 @@
 import './KanaDisplay.css'
 var kanas = require('../../kanas/kanas.json')
 var words = require('../../kanas/words.json')
+    
+const merged = {...kanas.kanas, ...words.words}
 
-function pickRandonKanaOrWord() {
-    var obj_keys = Object.keys(kanas.kanas);
+function pickRandonKana() {
+    var obj_keys = Object.keys(merged);
     var ran_obj = obj_keys[Math.floor(Math.random() *obj_keys.length)];
-    return kanas.kanas[ran_obj].kana
+    return merged[ran_obj].kana
 }
 
 const KanaDisplay = () => {
     return (
         <div className='kana-display-container'>
-            <h1>{pickRandonKanaOrWord()}</h1>
+            <h1>{pickRandonKana()}</h1>
         </div>
     )
 };
